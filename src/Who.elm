48ec4645f572d5e7ba6bdaf6ccdc4ee54f5abc73
Willy Unterkoefler <w.unterkoefler@gmail.com>
2020-07-09 19:02:31 -0400

    small wording change

--- src/Who.elm
+++ src/Who.elm
@@ -12,5 +12,5 @@ content =
 
 paragraphs : List String
 paragraphs =
-    [ """I am Willy Unterkoefler. Unless otherwise noted, I write the things on here, which explains their high quality. I also designed this website, which explains its... quality. If you want to know more about me, please read everything I post here and psychoanalyze it. To help you get started, the giraffe is really a metaphor for my healthy relationship with my father and three horrible years of speech therapy in elementary school. Despite my years speech therapy, you should know that I still pronounce water as "woodder". The rest of my life and personality can be derived."""
+    [ """I am Willy Unterkoefler. Unless otherwise noted, I write the things on here, which explains their high quality. I also designed this website, which explains its... quality. If you want to know more about me, please read everything I post here and psychoanalyze it. To help you get started, the giraffe is really a metaphor for my healthy relationship with my father and the lightning is a stand-in for my three horrible years of speech therapy in elementary school. Despite my years speech therapy, you should know that I still pronounce water as "woodder". The rest of my life and personality can be derived."""
     ]
